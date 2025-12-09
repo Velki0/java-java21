708bfbcc94462d3a51c5f5aeb0e60ccbc6f79ee0
Velki0 <b.gerardin125@gmail.com>
2025-12-09 11:47:41 +0100

    Correctif Exercice 2

--- src/test/java/java17/ex02/Method_02_Test.java
+++ src/test/java/java17/ex02/Method_02_Test.java
@@ -41,7 +41,7 @@ public class Method_02_Test {
         @Override
         public String format() {
 
-            return "DaoA[" + findAll().size() + " persons]";
+            return "DaoA" + IDao.super.format();
 
         }
         // TODO la méthode retourne une chaîne de la forme DaoA[<nb_personnes> persons]
