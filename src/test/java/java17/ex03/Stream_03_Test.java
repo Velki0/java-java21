96ec2502e35325e2e5cd96c5a9332ffd0fd05b93
Velki0 <b.gerardin125@gmail.com>
2025-12-09 16:18:40 +0100

    Réalisation TP

--- src/test/java/java17/ex03/Stream_03_Test.java
+++ src/test/java/java17/ex03/Stream_03_Test.java
@@ -4,7 +4,6 @@ import org.junit.Test;
 
 import java17.data.Data;
 import java17.data.domain.Customer;
-import java17.data.domain.Gender;
 import java17.data.domain.Order;
 import java17.data.domain.Pizza;
 
@@ -13,8 +12,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import java.util.stream.Collectors;
-
-import static java.util.stream.Collectors.*;
 
 import static org.hamcrest.Matchers.*;
 import static org.junit.Assert.*;
@@ -30,7 +27,7 @@ public class Stream_03_Test {
 		List<Order> orders = new Data().getOrders();
 
 		// TODO Retrouver la commande avec le prix le plus élevé
-		Optional<Order> result = null;
+		Optional<Order> result = orders.stream().max(Comparator.comparing(Order::getPrice));
 
 		assertThat(result.isPresent(), is(true));
 		assertThat(result.get().getPrice(), is(2200.0));
@@ -42,7 +39,7 @@ public class Stream_03_Test {
 		List<Order> orders = new Data().getOrders();
 
 		// TODO Retrouver la commande avec le prix le moins élevé
-		Optional<Order> result = null;
+		Optional<Order> result = orders.stream().min(Comparator.comparing(Order::getPrice));
 
 		assertThat(result.isPresent(), is(true));
 		assertThat(result.get().getPrice(), is(1000.0));
@@ -53,9 +50,8 @@ public class Stream_03_Test {
 
 		List<Customer> customers = new Data().getCustomers();
 
-		// TODO construire une chaîne contenant les prénoms des clients triés et séparés
-		// par le caractère "|"
-		String result = null;
+		// TODO construire une chaîne contenant les prénoms des clients triés et séparés par le caractère "|"
+		String result = customers.stream().map(Customer::getFirstname).sorted().collect(Collectors.joining("|"));
 
 		assertThat(result, is("Alexandra|Cyril|Johnny|Marion|Sophie"));
 	}
@@ -66,7 +62,7 @@ public class Stream_03_Test {
 		List<Order> orders = new Data().getOrders();
 
 		// TODO Extraire la liste des pizzas de toutes les commandes
-		List<Pizza> result = null;
+		List<Pizza> result = orders.stream().map(Order::getPizzas).flatMap(List::stream).toList();
 
 		assertThat(result.size(), is(9));
 	}
@@ -77,7 +73,7 @@ public class Stream_03_Test {
 		List<Order> orders = new Data().getOrders();
 
 		// TODO Extraire la liste des différentes pizzas de toutes les commandes
-		List<Pizza> result = null;
+		List<Pizza> result = orders.stream().map(Order::getPizzas).flatMap(List::stream).distinct().toList();
 
 		assertThat(result.size(), is(4));
 	}
@@ -88,7 +84,7 @@ public class Stream_03_Test {
 		List<Order> orders = new Data().getOrders();
 
 		// TODO construire une Map <Client, Commandes effectuées par le client
-		Map<Customer, List<Order>> result = null;
+		Map<Customer, List<Order>> result = orders.stream().collect(Collectors.groupingBy(Order::getCustomer));
 
 		assertThat(result.size(), is(2));
 		assertThat(result.get(new Customer(1)), hasSize(4));
@@ -102,7 +98,7 @@ public class Stream_03_Test {
 		// TODO Séparer la liste des pizzas en 2 ensembles :
 		// TODO true -> les pizzas dont le nom commence par "L"
 		// TODO false -> les autres
-		Map<Boolean, List<Pizza>> result = null;
+		Map<Boolean, List<Pizza>> result = pizzas.stream().collect(Collectors.partitioningBy(pizza -> pizza.getName().charAt(0) == 'L'));
 
 		assertThat(result.get(true), hasSize(6));
 		assertThat(result.get(false), hasSize(2));
