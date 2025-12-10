950a8491e90b70ce0830f4afecb7cd47eed3fa67
Velki0 <b.gerardin125@gmail.com>
2025-12-10 14:30:00 +0100

    Correctif Ex 4

--- src/test/java/java17/ex04/Function_04_Test.java
+++ src/test/java/java17/ex04/Function_04_Test.java
@@ -66,7 +66,7 @@ public class Function_04_Test {
 
         // TODO invoquer la méthode filter pour que le test soit passant
         // TODO chaîner les prédicats adult, lastnameIsFrance et firstnameIsArmor avec la méthode and
-        List<Person> result = filter(personList, lastnameIsFrance.and(firstnameIsArmor));
+        List<Person> result = filter(personList, adult.and(lastnameIsFrance.and(firstnameIsArmor)));
 
         assert result.size() == 1;
         assert result.get(0).getFirstname().equals("Armor");
