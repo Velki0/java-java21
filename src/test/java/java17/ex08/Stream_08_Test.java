8e1f3e8fa953555d00d166880c0736d9be2c3fd1
Velki0 <b.gerardin125@gmail.com>
2025-12-10 10:40:42 +0100

    Réalisation TP Ex 8

--- src/test/java/java17/ex08/Stream_08_Test.java
+++ src/test/java/java17/ex08/Stream_08_Test.java
@@ -4,8 +4,12 @@ import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
 
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Comparator;
 import java.util.Map;
 import java.util.Optional;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import org.junit.Test;
@@ -16,7 +20,7 @@ import org.junit.Test;
 public class Stream_08_Test {
 
     // Chemin vers un fichier de données des naissances
-    private static final String NAISSANCES_DEPUIS_1900_CSV = "./naissances_depuis_1900.csv";
+    private static final String NAISSANCES_DEPUIS_1900_CSV = "src/main/resources/naissances_depuis_1900.csv";
 
     // Structure modélisant les informations d'une ligne du fichier
     class Naissance {
@@ -61,11 +65,10 @@ public class Stream_08_Test {
 
         // TODO utiliser la méthode java.nio.file.Files.lines pour créer un stream de lignes du fichier naissances_depuis_1900.csv
         // Le bloc try(...) permet de fermer (close()) le stream après utilisation
-        try (Stream<String> lines = null) {
+        try (Stream<String> lines = Files.lines(Paths.get(NAISSANCES_DEPUIS_1900_CSV))) {
 
             // TODO construire une MAP (clé = année de naissance, valeur = somme des nombres de naissance de l'année)
-            Map<String, Integer> result = null;
-
+            Map<String, Integer> result = lines.skip(1).collect(Collectors.groupingBy(line -> line.substring(line.indexOf(";") + 1, line.indexOf(";") + 5), Collectors.summingInt(line -> Integer.parseInt(line.substring(line.lastIndexOf(';') + 1)))));
 
             assertThat(result.get("2015"), is(8097));
             assertThat(result.get("1900"), is(5130));
@@ -77,10 +80,13 @@ public class Stream_08_Test {
 
         // TODO utiliser la méthode java.nio.file.Files.lines pour créer un stream de lignes du fichier naissances_depuis_1900.csv
         // Le bloc try(...) permet de fermer (close()) le stream après utilisation
-        try (Stream<String> lines = null) {
+        try (Stream<String> lines = Files.lines(Paths.get(NAISSANCES_DEPUIS_1900_CSV))) {
 
             // TODO trouver l'année où il va eu le plus de nombre de naissance
-            Optional<Naissance> result = null;
+
+            Optional<Naissance> result = lines.skip(1)
+                                              .map(line -> new Naissance(line.substring(line.indexOf(";") + 1, line.indexOf(";") + 5), line.substring(line.indexOf(";") + 6, line.lastIndexOf(";")), Integer.parseInt(line.substring(line.lastIndexOf(';') + 1))))
+                                              .max(Comparator.comparingInt(Naissance::getNombre));
 
 
             assertThat(result.get().getNombre(), is(48));
@@ -93,11 +99,14 @@ public class Stream_08_Test {
     public void test_collectingAndThen() throws IOException {
         // TODO utiliser la méthode java.nio.file.Files.lines pour créer un stream de lignes du fichier naissances_depuis_1900.csv
         // Le bloc try(...) permet de fermer (close()) le stream après utilisation
-        try (Stream<String> lines = null) {
+        try (Stream<String> lines = Files.lines(Paths.get(NAISSANCES_DEPUIS_1900_CSV))) {
 
             // TODO construire une MAP (clé = année de naissance, valeur = maximum de nombre de naissances)
             // TODO utiliser la méthode "collectingAndThen" à la suite d'un "grouping"
-            Map<String, Naissance> result = null;
+            Map<String, Naissance> result = lines.skip(1)
+                    .map(line -> new Naissance(line.substring(line.indexOf(";") + 1, line.indexOf(";") + 5), line.substring(line.indexOf(";") + 6, line.lastIndexOf(";")), Integer.parseInt(line.substring(line.lastIndexOf(';') + 1))))
+                    .collect(Collectors.groupingBy(Naissance::getAnnee, Collectors.collectingAndThen(Collectors.maxBy(Comparator.comparingInt(Naissance::getNombre)), Optional::get))
+                    );
 
             assertThat(result.get("2015").getNombre(), is(38));
             assertThat(result.get("2015").getJour(), is("20150909"));
